8e1bd32b6227558d9d76d088dd429d03c57dc191
peter molnar <pm@aston.sk>
2017-11-01 18:48:09 +0100

    add @jsx-arrow anotation

--- src/index.js
+++ src/index.js
@@ -14,13 +14,36 @@
 export default function ({ types: t }) {
 
   t.react.isCompatTag = function(tagName){ return true };
+  
+  const JSX_ANNOTATION_REGEX = /\*?\s*@jsx-arrow\s+([^\s]+)/;
+  var params = [];
+
 
   const visitor = {
-  
+ 
+  	Program(path, state) { 
+
+		const { file } = state; 
+		let sparams = state.opts.pragma;
+
+		for(const comment of file.ast.comments) {
+      		const matches = JSX_ANNOTATION_REGEX.exec(comment.value);
+      		if (matches) { 
+      			sparams = matches[1];
+	          	break;
+	      	}
+        }
+
+        if(sparams) {
+        	params = sparams.split(',').map((e) => e.trim());
+        }
+
+  	},
+
   	JSXExpressionContainer(path) {
 		if(path.parent && (path.parent.type=='JSXAttribute' || path.parent.type=='JSXElement')){
 			if(!path.node._kox){
-				path.node.expression = t.arrowFunctionExpression([t.identifier('m')], path.node.expression, false);
+				path.node.expression = t.arrowFunctionExpression(params.map((n)=> t.identifier(n)), path.node.expression, false);
 				path.node._kox = true;
 			}
 		}
